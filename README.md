dabcc0b507fbc1def2694de78a3029366b696e3d
Wali Muhammad <wm401238@gmail.com>
2024-03-17 09:44:14 +0500

    Form is complete reactive

--- README.md
+++ README.md
@@ -1,27 +1,22 @@
 # ReactiveApproachForm
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.7.
+## Reactive Setup
+Import ReactiveFormsModule in your module:
 
-## Development server
+<code>
+import { ReactiveFormsModule } from '@angular/forms';
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
+@NgModule({
+  imports: [
+    ReactiveFormsModule
+  ],
+  // other module configurations
+})
+export class AppModule { }
+</code>
 
-## Code scaffolding
+<code>
+import { FormGroup, FormControl } from '@angular/forms';
+  myForm: FormGroup;
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
-## Build
-
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+</code>
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators, FormArray, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'reactive-approach-form';
+export class AppComponent implements OnInit {
+  signupForm!: FormGroup; // Declare the form group
+  forbiddenUserNames = ['Chris', 'John']; // Corrected variable name and array
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    // Initialize the form using FormBuilder
+    this.signupForm = this.formBuilder.group({
+      userData: this.formBuilder.group({
+        username: [null, [Validators.required, this.forbiddenNameValidator.bind(this)]], // Use custom validator function
+        email: [null, [Validators.required, Validators.email]], // Email form control with required and email validators
+      }),
+      gender: ['male'], // Gender form control with initial value 'male'
+      hobbies: this.formBuilder.array([]), // Initialize hobbies as a form array
+    });
+  }
+
+  // Getter to access hobbies form array
+  get hobbies(): FormArray {
+    return this.signupForm.get('hobbies') as FormArray;
+  }
+
+  // Method to add a hobby form control to the hobbies form array
+  addHobby(): void {
+    this.hobbies.push(this.formBuilder.control(null, Validators.required));
+    // Add a new hobby form control with required validator
+  }
+
+  // Custom validator function to check if username is forbidden
+  forbiddenNameValidator(control: FormControl): { [s: string]: boolean } | null {
+    if (this.forbiddenUserNames.includes(control.value)) {
+      return { 'nameForbidden': true }; // Return an error object if username is forbidden
+    }
+    return null; // Return null if username is allowed
+  }
+
+    // Helper method to cast AbstractControl to FormControl
+    castToFormControl(control: AbstractControl): FormControl {
+      return control as FormControl; // Cast AbstractControl to FormControl
+    }
+
+  // Form submission handler
+  SubmitForm(): void {
+    if (this.signupForm.valid) {
+      console.log(this.signupForm.value); // Log form value when valid
+      // You can submit the form data to your backend or perform other actions here
+    } else {
+      console.log("Form is invalid"); // Log message when form is invalid
+    }
+  }
 }
